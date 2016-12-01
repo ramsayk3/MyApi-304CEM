ead6037777fce12140cf2a49b1287435518db067
ramsayk3 <kairamsay@Kai-Ramsays-MacBook-Pro.local>
2016-12-01 16:00:51 +0000

    Updated the post method for favourites
    didn’t need next
    needed to be send not save

--- server.js
+++ server.js
@@ -2,30 +2,31 @@
 const restify = require('restify')
 const server = restify.createServer()
 const omdb = require('./modules/film')
-const movieDb = require('../Schema/Schema')
+const movieDb = require('./Schema/Schema')
 const persist = require('./modules/persistence')
-
 server.use(restify.queryParser())
 server.use(restify.bodyParser())
 server.use(restify.acceptParser(server.acceptable))
-
+server.use(restify.CORS())
 const defaultPort = 8080
-
-server.get('/search', function(req, res, next) {
+server.get('/search', function (req, res, next) {
+    console.log("*** Searching For ***")
     console.log(req.params.s)
-    omdb.searchMovie(req.params.s, function(err, result){
-        if(err){
+    omdb.searchMovie(req.params.s, function (err, result) {
+        if (err) {
             console.log(err)
-        }else{
+        }
+        else {
             res.send(result)
         }
+        console.log("*** Here Are Your Results ***")
+        console.log(result)
     })
 })
-
-server.get('/movie', function(req, res, next) {
+server.get('/movie', function (req, res, next) {
     let input
     let type
-    if(req.params.n !== undefined){
+    if (req.params.n !== undefined) {
         input = req.params.n
         type = 't'
     }
@@ -33,34 +34,39 @@ server.get('/movie', function(req, res, next) {
         input = req.params.i
         type = 'i'
     }
-    omdb.getMovie(input, type, req.params.y, function(err, result){
-        if(err){
+    omdb.getMovie(input, type, req.params.y, function (err, result) {
+        if (err) {
             console.log(err)
-        }else{
+        }
+        else {
+            res.send(result)
+        }
+    })
+})
+server.post('/favourites', function (req, res) {
+    console.log('adding ' + '' + req.params.i)
+    omdb.addMovie(req.params.i, function (err, result) {
+        console.log(result)
+        if (err) {
+            console.log(err)
+        }
+        else {
             res.send(result)
         }
     })
 })
 
-server.post('/favourites', function (req, res, next) {
-    console.log('adding ' + '' + req.params.i)
-    omdb.addMovie(req.params.i, function (err, result) {
-        if (err) {
-            console.log(err)
-        }
-        else {
-            res.save(result)
-        }
-    })
-})
 
-
-
+server.get("/favourites", function (req, res, next) {
+    res.send("You will see all the products in the colection with this end point");
+    return next();
+});
 const port = process.env.PORT || defaultPort
-server.listen(port, function(err) {
-	if (err) {
-		console.error(err)
-	} else {
-		console.log('App is ready at : ' + port)
-	}
+server.listen(port, function (err) {
+    if (err) {
+        console.error(err)
+    }
+    else {
+        console.log('App is ready at : ' + port)
+    }
 })
