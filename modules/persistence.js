a6563acacfe17ac374b720ac842614c31206f6b8
ramsayk3 <kairamsay@Kai-Ramsays-MacBook-Pro.local>
2016-12-01 16:01:29 +0000

    changed to callback rather than promise

--- modules/persistence.js
+++ modules/persistence.js
@@ -2,16 +2,16 @@
 
 const schema = require('../Schema/Schema')
 
-exports.saveMovie = movieData => new promise( (resolve, reject)=>{
+exports.saveMovie = (movieData, callback) =>{
     if (!'title' in movieData && !'year' in movieData && !'plot' in movieData && !'imdbRating' in movieData && !'imdbID' in movieData){
-        reject(new Error('Not a correct movie object'))
+        callback(new Error('Not a correct movie object'))
     }
     const movie = new schema.Movie(movieData)
     
     movie.save( (err, movie)=> {
         if (err) {
-            reject(new Error('Could not save movie'))
+            callback(new Error('Could not save movie'))
         }
-        resolve(movie)
+        callback(null, movie)
     })
-})
+}
