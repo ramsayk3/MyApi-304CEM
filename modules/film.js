304e5528623624e8d0dcfe802caf09b5b592f1ee
ramsayk3 <ramsayk3@uni.coventry.ac.uk>
2016-12-07 02:27:09 +0000

    Updated to allow getbyid/deletebyid. Cleaned functions
    added parameter to allow user to get by imdb and also delete by imdb.
    Also moved database functions from film.js to persistence.js.

--- modules/film.js
+++ modules/film.js
@@ -1,60 +1,113 @@
 const request = require("request")
+const schema = require("../Schema/Schema.js")
+const db = require('./persistence.js')
+var ObjectId = require('mongodb').ObjectID
 
-
-exports.searchMovie = function(movieSearch, callback) {
-    const search = { method: 'GET',
-        url: 'http://www.omdbapi.com/',
-        qs: { s: movieSearch, r: 'json' },
-    }
-  request(search, function (error, response, body) {
-        if (error) throw new Error(error)
-        return callback(null, JSON.parse(body))
-  })
-}
-
-exports.getMovie = function(input, type, year, callback) {
-    request(`http://www.omdbapi.com/?${type}=${input}&y=${year}`,
-            function (error, response, body) {
-        
+exports.searchMovie = function (movieSearch, callback) {
+    const search = {
+        method: 'GET'
+        , url: 'http://www.omdbapi.com/'
+        , qs: {
+            s: movieSearch
+            , r: 'json'
+        }
+    , }
+    request(search, function (error, response, body) {
         if (error) throw new Error(error)
         return callback(null, JSON.parse(body))
     })
 }
-exports.addMovie = function(imdbID, callback) {
-	const url = `'http://www.omdbapi.com/?i=${imdbID}`
+exports.getMovie = function (input, type, year, callback) {
+    request(`http://www.omdbapi.com/?${type}=${input}&y=${year}`, function (error, response, body) {
+        if (error) throw new Error(error)
+        return callback(null, JSON.parse(body))
+    })
+}
+exports.addMovie = function (imdbID, callback) {
+    const url = `http://www.omdbapi.com/?i=${imdbID}`
+    console.log(url)
+    request.get(url, (err, res, body) => {
+        if (err) return callback(Error('could not complete request'))
+        const json = JSON.parse(body)
+        if (json.totalItems === 0) {
+            console.log('no results')
+            return callback(Error('movie not found'))
+        }
+        console.log('define the movie to add')
+        const data = {
+            title: json.Title
+            , Year: json.Year
+            , Plot: json.Plot
+            , imdbID: json.imdbID
+        }
+        console.log('Adding movie to your favourites')
+        db.saveMovie(json, function (err, movie) {
+            console.log('attempt made')
+            if (err) {
+                console.log('an error adding movie')
+                callback(Error(`database error: ${err}`))
+            }
+            console.log('movie added')
+            console.log(movie)
+            return callback(null, movie)
+        })
+    })
+}
+exports.showFavourites = function (err, callback) {
+    console.log('Fetching the favourites movie list')
+    schema.Movie.find({}, function (err, movies) {
+        if (err) {
+            console.log('could not fetch movies')
+            throw err
+        }
+        console.log(movies)
+        return callback(null, movies)
+    })
+}
 
-	request.get( url, (err, res, body) => {
-		if (err) return callback(Error('could not complete request'))
-		const json = JSON.parse(body)
 
-		if (json.totalItems === 0) {
-			console.log('no results')
-			return callback(Error('movie not found'))
+exports.showFavouritebyid = function(id, callback) {
+	console.log('retrieving the film by id')
+	schema.Movie.find({imdbID : id}, function(err, movies) {
+		if (err) {
+			console.log('error finding film')
+			throw err
 		}
-		const data = {
-			title: json.Title[0],
-			Year: json.Year[0],
-			Plot: json.Plot[0],
-            imdbID: json.imdbID[0]
-		}
-
-		console.log('define the movie to add')
-		const book = new Book({
-			title: data.title,
-			authors: data.authors,
-			description: data.description
-		})
-
-		console.log('Adding movie to your favourites')
-		movieSchema.save( function(err, movie) {
-			console.log('attempt made')
-			if (err) {
-				console.log('an error adding movie')
-				callback( Error(`database error: ${err}`) )
-			}
-			console.log('movie added')
-			console.log(movie)
-			return callback(null, movie)
-		})
+  	console.log(movies)
+    return callback(null, movies)
 	})
 }
+
+exports.remove = function(id, callback) {
+	console.log('Deleting the film')
+	schema.Movie.remove({imdbID : id }, function(err, movies) {
+		if (err) {
+			console.log('error finding film')
+			throw err
+		}
+
+  	console.log(movies)
+    return callback(null, movies)
+	})
+}
+
+
+
+
+
+
+
+
+/*
+exports.remove = function (imdbID, callback) {
+    schema.Movie.remove({imdbID:''})
+        , function (err, movies) {
+            if (err) {
+                console.log('could not delete movie')
+                throw err
+            }
+            console.log(movies)
+            return callback(null, movies)
+        }
+}
+*/
--- modules/persistence.js
+++ modules/persistence.js
@@ -1,17 +1,53 @@
 'use strict'
-
 const schema = require('../Schema/Schema')
-
-exports.saveMovie = movieData => new promise( (resolve, reject)=>{
-    if (!'title' in movieData && !'year' in movieData && !'plot' in movieData && !'imdbRating' in movieData && !'imdbID' in movieData){
-        reject(new Error('Not a correct movie object'))
+const schema1 = require('../Schema/Schema').Movie
+const omdb = require('./film')
+exports.saveMovie = (movieData, callback) => {
+    if (!'title' in movieData && !'year' in movieData && !'plot' in movieData && !'imdbRating' in movieData && !'imdbID' in movieData) {
+        callback(new Error('Not a correct movie object'))
     }
     const movie = new schema.Movie(movieData)
-    
-    movie.save( (err, movie)=> {
+    movie.save((err, movie) => {
         if (err) {
-            reject(new Error('Could not save movie'))
+            callback(new Error('Could not save movie'))
         }
-        resolve(movie)
+        callback(null, movie)
     })
-})
+}
+exports.showFavourites = function (err, callback) {
+    console.log('Fetching the favourites movie list')
+    schema.Movie.find({}, function (err, movies) {
+        if (err) {
+            console.log('could not fetch movies')
+            throw err
+        }
+        console.log(movies)
+        return callback(null, movies)
+    })
+}
+exports.showFavouritebyid = function (id, callback) {
+    console.log('retrieving the film by id')
+    schema.Movie.find({
+        imdbID: id
+    }, function (err, movies) {
+        if (err) {
+            console.log('error finding film')
+            throw err
+        }
+        console.log(movies)
+        return callback(null, movies)
+    })
+}
+exports.remove = function (id, callback) {
+        console.log('Deleting the film')
+        schema.Movie.remove({
+            imdbID: id
+        }, function (err, movies) {
+            if (err) {
+                console.log('error finding film')
+                throw err
+            }
+            console.log(movies)
+            return callback(null, movies)
+        })
+    }
