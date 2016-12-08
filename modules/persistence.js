68d06631ce11efc473de0bbb58537b7933bf6dc0
ramsayk3 <ramsayk3@uni.coventry.ac.uk>
2016-12-08 02:27:03 +0000

    Updated to deny user creating duplicates

--- modules/persistence.js
+++ modules/persistence.js
@@ -1,48 +1,64 @@
 'use strict'
 const schema = require('../Schema/Schema')
-
 exports.saveMovie = (movieData, callback) => {
-	if (!'title' in movieData && !'year' in movieData && !'plot' in movieData && !'imdbRating' in movieData && !'imdbID' in movieData) {
-		callback(new Error('Not a correct movie object'))
-	}
-	const movie = new schema.Movie(movieData)
-
-	movie.save((err, movie) => {
-		if (err) {
-			callback(new Error('Could not save movie'))
-		}
-		callback(null, movie)
-	})
+    if (!'title' in movieData && !'year' in movieData && !'plot' in movieData && !'imdbRating' in movieData && !'imdbID' in movieData) {
+        callback(new Error('Not a correct movie object'))
+    }
+    const movie = new schema.Movie(movieData)
+    schema.Movie.find({
+        imdbID: movieData.imdbID
+    }, function (err, movies) {
+        if (movies.length === 0) {
+            movie.save((err, movie) => {
+                if (err) {
+                    callback(new Error('Could not save movie'))
+                }
+                callback(null, movie)
+            })
+        }
+        else {
+            callback('Denied as duplicate')
+        }
+    })
 }
-exports.showFavourites = function(callback) {
-	schema.Movie.find({}, function(err, movies) {
-		if (err) {
-			throw err
-		}
-		return callback(null, movies)
-	})
+exports.showFavourites = function (callback) {
+    schema.Movie.find({}, function (err, movies) {
+        if (err) {
+            throw err
+        }
+        return callback(null, movies)
+    })
 }
-exports.showFavouritebyid = function(id, callback) {
-	schema.Movie.find({imdbID: id}, function(err, movies) {
-		if (err) {
-			throw err
-		}
-		return callback(null, movies)
-	})
+exports.showFavouritebyid = function (id, callback) {
+    schema.Movie.find({
+        imdbID: id
+    }, function (err, movies) {
+        console.log(movies)
+        if (err) {
+            throw err
+        }
+        return callback(null, movies)
+    })
 }
-exports.remove = function(id, callback) {
-	schema.Movie.remove({imdbID: id}, function(err, movies) {
-		if (err) {
-			throw err
-		}
-		return callback(null, movies)
-	})
+exports.remove = function (id, callback) {
+    schema.Movie.remove({
+        imdbID: id
+    }, function (err, movies) {
+        if (err) {
+            throw err
+        }
+        return callback(null, movies)
+    })
 }
-exports.updaterating = function(id, imdbRating, callback) {
-	schema.Movie.findOneAndUpdate({imdbID: id}, {'imdbRating': imdbRating}, function(err, movies) {
-		if (err) {
-			throw err
-		}
-		return callback(null, movies)
-	})
+exports.updaterating = function (id, imdbRating, callback) {
+    schema.Movie.findOneAndUpdate({
+        imdbID: id
+    }, {
+        'imdbRating': imdbRating
+    }, function (err, movies) {
+        if (err) {
+            throw err
+        }
+        return callback(null, movies)
+    })
 }
