d2f10040a6063bde858e7bf2aee6c86f5674a5eb
ramsayk3 <ramsayk3@uni.coventry.ac.uk>
2016-12-08 01:15:33 +0000

    update

--- server.js
+++ server.js
@@ -4,105 +4,100 @@ const server = restify.createServer()
 const omdb = require('./modules/film')
 const movieDb = require('./Schema/Schema')
 const persist = require('./modules/persistence')
+
 server.use(restify.queryParser())
 server.use(restify.bodyParser())
 server.use(restify.acceptParser(server.acceptable))
 server.use(restify.CORS())
 const defaultPort = 8080
 
-server.get('/search', function (req, res, next) {
-    omdb.searchMovie(req.params.s, function (err, result) {
-        if (err) {
-            res.send(400,err)
-        }
-        else {
-            res.send(result)
-        }
-    })
+server.get('/search', function(req, res, next) {
+	omdb.searchMovie(req.params.s, function(err, result) {
+		if (err) {
+			res.send(400,err)
+		}		else {
+			res.send(result)
+		}
+	})
 })
-server.get('/movie', function (req, res, next) {
-    let input
-    let type
-    if (req.params.n !== undefined) {
-        input = req.params.n
-        type = 't'
-    }
-    else {
-        input = req.params.i
-        type = 'i'
-    }
-    omdb.getMovie(input, type, req.params.y, function (err, result) {
-        if (err) {
-            res.send(400,err)
-        }
-        else {
-            res.send(result)
-        }
-    })
+server.get('/movie', function(req, res, next) {
+	let input
+	let type
+
+	if (req.params.n !== undefined) {
+		input = req.params.n
+		type = 't'
+	}	else {
+		input = req.params.i
+		type = 'i'
+	}
+	omdb.getMovie(input, type, req.params.y, function(err, result) {
+		if (err) {
+			res.send(400,err)
+		}		else {
+			res.send(result)
+		}
+	})
 })
-server.post('/favourites', function (req, res) {
-    console.log('adding ' + '' + req.params.i)
-    omdb.addMovie(req.params.i, function (err, result) {
-        console.log(result)
-        if (err) {
-            res.send(400,err)
-        }
-        else {
-            res.send(201,result)
-        }
-    })
+server.post('/favourites', function(req, res) {
+	console.log('adding ' + '' + req.params.i)
+	omdb.addMovie(req.params.i, function(err, result) {
+		console.log(result)
+		if (err) {
+			res.send(400,err)
+		}		else {
+			res.send(201,result)
+		}
+	})
 })
 server.get('/favourites', (req, res) => {
-    persist.showFavourites(req, (err, data) => {
-        res.setHeader('accepts', 'GET, POST')
-        if (err) {
-            res.send(400,err)
-        }
-        else {
-            res.send(data)
-        }
-    })
+	persist.showFavourites(req, (err, data) => {
+		res.setHeader('accepts', 'GET, POST')
+		if (err) {
+			res.send(400,err)
+		}		else {
+			res.send(data)
+		}
+	})
 })
 server.get('/favourites/:id', (req, res) => {
-    const id = req.params.id
-    persist.showFavouritebyid(req.params.id, (err, data) => {
-        res.setHeader('accepts', 'GET, POST')
-        if (err) {
-            res.send(400,err)
-        }
-        else {
-            res.send(data)
-        }
-    })
+	const id = req.params.id
+
+	persist.showFavouritebyid(req.params.id, (err, data) => {
+		res.setHeader('accepts', 'GET, POST')
+		if (err) {
+			res.send(400,err)
+		}		else {
+			res.send(data)
+		}
+	})
 })
 server.del('/favourites/:id', (req, res) => {
-    persist.remove(req.params.id, (err, data) => {
-        res.setHeader('accepts', 'GET, DELETE')
-        if (err) {
-            res.send(400,err)
-        }
-        else {
-            res.send(data)
-        }
-    })
+	persist.remove(req.params.id, (err, data) => {
+		res.setHeader('accepts', 'GET, DELETE')
+		if (err) {
+			res.send(400,err)
+		}		else {
+			res.send(data)
+		}
+	})
 })
 server.put('/favourites/.*', (req, res) => {
-  persist.updaterating(req.params.id,req.params.rating, (err, data) => {
-        res.setHeader('accepts', 'PUT, POST')
-        if (err) {
-            res.send(400,err)
-        }
-        else {
-            res.send(data)
-        }
-    })
+	persist.updaterating(req.params.id,req.params.rating, (err, data) => {
+		res.setHeader('accepts', 'PUT, POST')
+		if (err) {
+			res.send(400,err)
+		}		else {
+			res.send(data)
+		}
+	})
 })
 const port = process.env.PORT || defaultPort
-server.listen(port, function (err) {
-    if (err) {
-        console.error(err)
-    }
-    else {
-        console.log('App is ready at : ' + port)
-    }
+
+server.listen(port, function(err) {
+	if (err) {
+		console.error(err)
+	}	else {
+		console.log('App is ready at : ' + port)
+	}
 })
