afdd86f421300896f607b7bf47c3f0f60b73a8fa
ramsayk3 <ramsayk3@uni.coventry.ac.uk>
2016-12-08 15:26:05 +0000

    Frisky test edits

--- Spec/Server-spec.js
+++ Spec/Server-spec.js
@@ -1,6 +1,16 @@
 const frisby = require('frisby')
 const port = 8080
 
+/*frisby.create('Post Movie To Favourites')
+  .post('http://localhost:' + port + '/favourites?i=tt0435761', { strictSSL: false })
+    .expectStatus(200)
+ .expectJSON({
+  Title: 'Toy Story 3',
+  Year: 2010
+  })
+ .toss()
+ */
+
 frisby.create('Get Movie By Specific ImdbID')
   .get('http://localhost:' + port + '/movie?i=tt0114709', { strictSSL: false })
     .expectStatus(200)
@@ -29,20 +39,32 @@ frisby.create('Is There Data In My Database')
  .toss()
 
 frisby.create('Get Data From My Database')
-  .get('http://localhost:' + port + '/favourites/:id?id=tt0387564', { strictSSL: false })
-    .expectStatus(200)
- .expectJSON([{
-  Title: 'Saw',
-     Year: 2004
-  }])
- .toss()
-
-/*frisby.create('Post Movie To Favourites')
-  .post('http://localhost:' + port + '/favourites?i=tt0435761', { strictSSL: false })
+  .get('http://localhost:8080/favourites/tt0120363', { strictSSL: false })
     .expectStatus(200)
  .expectJSON({
-  Title: 'Toy Story 3',
-  Year: 2010
+  Title: 'Toy Story 2',
+  Year: 1999
+  })
+ .toss()
+
+/*frisby.create('Get Data From My Database')
+ .put('http://localhost:8080/favourites/id=tt0120363&rating=9.9', { strictSSL: false })
+    .expectStatus(200)
+ .expectJSON([{
+  Title: 'Toy Story 2',
+  Year: 1999,
+imdbRating: 9.9
+  }])
+ .toss()
+ */
+
+
+/*frisby.create('Delete Data From My Database')
+  .delete('http://localhost:8080/favourites/:id?id=tt0120366', { strictSSL: false })
+    .expectStatus(200)
+ .expectJSON({
+  ok: 1
   })
  .toss()
  */
+
