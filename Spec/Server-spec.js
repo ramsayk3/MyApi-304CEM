c8988bbed696baf9873e9af3af1fa4d65bf84211
ramsayk3 <ramsayk3@uni.coventry.ac.uk>
2016-12-08 05:01:51 +0000

    Updated with Frisby tests

--- Spec/Server-spec.js
+++ Spec/Server-spec.js
@@ -1,21 +1,48 @@
-'use strict'
-const request = require("request")
-const base_url = "http://localhost:8080/"
-describe("Server Testing", function () {
-    describe("GET /search", function () {
-        it("returns status code 200", function (done) {
-            request.get(base_url + 'search?=Saw"', function (error, response, body) {
-                expect(response.statusCode).toBe(200);
-                done()
-            })
-        })
-    })
-    describe("GET /madeup", function () {
-        it("returns status code 404 not found", function (done) {
-            request.get(base_url + 'madeup', function (error, response, body) {
-                expect(response.statusCode).toBe(404);
-                done()
-            })
-        })
-    })
-})
+const frisby = require('frisby')
+const port = 8080
+
+frisby.create('Get Movie By Specific ImdbID')
+  .get('http://localhost:' + port + '/movie?i=tt0114709', { strictSSL: false })
+    .expectStatus(200)
+ .expectJSON({
+   Title: 'Toy Story',
+   Year: '1995'
+  })
+ .toss()
+
+frisby.create('Search Omdb Database')
+  .get('http://localhost:' + port + '/search?s=Toy%20Story', { strictSSL: false })
+    .expectStatus(200)
+ .expectJSON('Search', [{
+  Title: 'Toy Story',
+  Year: '1995'
+  }])
+ .toss()
+
+frisby.create('Is There Data In My Database')
+  .get('http://localhost:' + port + '/favourites', { strictSSL: false })
+    .expectStatus(200)
+ .expectJSON([{
+  Title: 'Toy Story 2',
+  Year: 1999
+  }])
+ .toss()
+
+frisby.create('Get Data From My Database')
+  .get('http://localhost:' + port + '/favourites/:id?id=tt0387564', { strictSSL: false })
+    .expectStatus(200)
+ .expectJSON([{
+  Title: 'Saw',
+     Year: 2004
+  }])
+ .toss()
+
+/*frisby.create('Post Movie To Favourites')
+  .post('http://localhost:' + port + '/favourites?i=tt0435761', { strictSSL: false })
+    .expectStatus(200)
+ .expectJSON({
+  Title: 'Toy Story 3',
+  Year: 2010
+  })
+ .toss()
+ */
