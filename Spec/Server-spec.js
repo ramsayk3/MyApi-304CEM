d9d93f9b3f91c0ad82c37f10cb50c771ecd0250a
ramsayk3 <ramsayk3@uni.coventry.ac.uk>
2016-12-10 05:56:14 +0000

    Coverage

--- Spec/Server-spec.js
+++ Spec/Server-spec.js
@@ -1,7 +1,7 @@
 const frisby = require('frisby')
 const port = 8080
 
-frisby.create('Post Movie To Favourites')
+frisby.create('Post Movie')
   .post('http://localhost:' + port + '/favourites?i=tt0435761', { strictSSL: false })
     .expectStatus(200)
  .expectJSON({
@@ -9,9 +9,17 @@ frisby.create('Post Movie To Favourites')
   Year: 2010
   })
  .toss()
+
+frisby.create('Post Movie Error')
+  .post('http://localhost:' + port + '/favourites?i=', { strictSSL: false })
+    .expectStatus(500)
+ .expectJSON({
+  message: "movie not found"
+  })
+ .toss()
  
 
-frisby.create('Get Movie By Specific ImdbID')
+frisby.create('Get Movie By ID')
   .get('http://localhost:' + port + '/movie?i=tt0114709', { strictSSL: false })
     .expectStatus(200)
  .expectJSON({
@@ -20,7 +28,15 @@ frisby.create('Get Movie By Specific ImdbID')
   })
  .toss()
 
-frisby.create('Search Omdb Database')
+frisby.create('Get Movie By ID Error')
+  .get('http://localhost:' + port + '/movie?i=', { strictSSL: false })
+    .expectStatus(500)
+ .expectJSON({
+   message: "null"
+  })
+ .toss()
+
+frisby.create('Search')
   .get('http://localhost:' + port + '/search?s=Toy%20Story', { strictSSL: false })
     .expectStatus(200)
  .expectJSON('Search', [{
@@ -29,7 +45,16 @@ frisby.create('Search Omdb Database')
   }])
  .toss()
 
-frisby.create('Is There Data In My Database')
+frisby.create('Search Error')
+  .get('http://localhost:' + port + '/search?s=', { strictSSL: false })
+    .expectStatus(500)
+ .expectJSON({
+  message: "null"
+  })
+ .toss()
+
+
+frisby.create('Database')
   .get('http://localhost:' + port + '/favourites', { strictSSL: false })
     .expectStatus(200)
  .expectJSON([{
@@ -38,7 +63,17 @@ frisby.create('Is There Data In My Database')
   }])
  .toss()
 
-frisby.create('Get Data From My Database')
+/*frisby.create('Database Error')
+  .get('http://localhost:' + port + '/favourites', { strictSSL: false })
+    .expectStatus(200)
+ .expectJSON([{
+  Title: 'Toy Story 2',
+  Year: 1999
+  }])
+ .toss()
+ */
+
+frisby.create('My Database')
   .get('http://localhost:8080/favourites/tt0435761', { strictSSL: false })
     .expectStatus(200)
  .expectJSON({
@@ -47,12 +82,28 @@ frisby.create('Get Data From My Database')
   })
  .toss()
 
-frisby.create('Get Data From My Database')
- .put('http://localhost:8080/favourites/?id=tt0435761&rating=9.9', { strictSSL: false })
+frisby.create('My Database Error')
+  .get('http://localhost:8080/favourites/tt04357', { strictSSL: false })
+    .expectStatus(500)
+ .expectJSON({
+  message: ''
+  })
+ .toss()
+
+frisby.create('Update Rating')
+ .put('http://localhost:8080/favourites/?id=tt0120363&rating=9.9', { strictSSL: false })
     .expectStatus(200)
  .expectJSON({
   Title: 'Toy Story 3',
   Year: 2010
+  })
+ .toss()
+
+frisby.create('Update Rating Error')
+ .put('http://localhost:8080/favourites/?id=&rating=9.9', { strictSSL: false })
+    .expectStatus(500)
+ .expectJSON({
+  message: "Pass a valid id"
   })
  .toss()
 
@@ -63,6 +114,14 @@ frisby.create('Delete Data From My Database')
     .expectStatus(200)
  .expectJSON({
   ok: 1
+  })
+ .toss()
+
+frisby.create('Delete Error')
+  .delete('http://localhost:8080/favourites/:id?id=', { strictSSL: false })
+    .expectStatus(200)
+ .expectJSON({
+ message: ''
   })
  .toss()
  
--- Spec/persistence-spec.js
+++ Spec/persistence-spec.js
@@ -1,32 +1,54 @@
 'use strict'
 const persist = require('../modules/persistence')
-
-xdescribe('Persistence Module Testing', function (){
-    xit('Should return favourites',function (done){
-        persist.showFavourites('',function (err, searchResult){
-        expect(err).toBe(null)
-        done()  
+xdescribe('Persistence Module Testing', function () {
+    it('Show By ID', function (done) {
+        persist.showFavouritebyid('tt0120363', function (err, searchResult) {
+            expect(err).toBe(null)
+            expect(searchResult.Title).toBe('Toy Story 2')
+            done()
         })
     })
-    it('Should return toy story 2',function (done){
-     persist.showFavouritebyid('tt0120363',function (err, searchResult){
-        expect(err).toBe(null)
-        expect(searchResult.Title).toBe('Toy Story 2')
-        done()  
+    it('Show By ID Error', function (done) {
+        persist.showFavouritebyid('tt0120', function (err, searchResult) {
+            expect(err).toBe(err)
+            done()
         })
     })
-    xit('Should return toy story 2',function (done){
-        persist.remove('tt0120363',function (err, searchResult){
-        expect(err).toBe(null)
-        expect(searchResult.Title).toBe('Toy Story 2')
-        done()  
+    it('Remove Film', function (done) {
+        persist.remove('tt1233227', function (err, searchResult) {
+            expect(err).toBe(null)
+            done()
         })
     })
-    xit('Should return toy story 2',function (done){
-        persist.updaterating('tt0120363',function (err, searchResult){
-        expect(err).toBe(null)
-        expect(searchResult.Title).toBe('Toy Story 2')
-        done()  
+    it('Remove Film Error', function (done) {
+        persist.remove('', function (err, searchResult) {
+            expect(err).toBe(err)
+            done()
+        })
+    })
+    it('Update Rating', function (done) {
+        persist.updaterating('tt0120363', 5, function (err, searchResult) {
+            expect(err).toBe(null)
+            expect(searchResult.Title).toBe('Toy Story 2')
+            done()
+        })
+    })
+    it('Update Rating Error', function (done) {
+        persist.updaterating('', 5, function (err, searchResult) {
+            expect(err).toBe(err)
+            done()
+        })
+    })
+    xit('Should return favourites', function (done) {
+        persist.showFavourites('', function (err, searchResult) {
+            expect(err).toBe(null)
+            done()
+        })
+    })
+    xit('Should return favourites', function (done) {
+        persist.showFavourites(123, function (err, searchResult) {
+            expect(err).toBe(null)
+            done()
         })
     })
 })
