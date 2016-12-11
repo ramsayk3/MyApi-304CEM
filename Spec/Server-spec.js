1a076ba5b6fc6693c84499120a4e6f4398aaf344
ramsayk3 <ramsayk3@uni.coventry.ac.uk>
2016-12-11 22:09:06 +0000

    Eslint Fixes

--- Spec/Server-spec.js
+++ Spec/Server-spec.js
@@ -5,54 +5,54 @@ frisby.create('Post Movie')
   .post('http://localhost:' + port + '/favourites?i=tt0435761', { strictSSL: false })
     .expectStatus(200)
  .expectJSON({
-  Title: 'Toy Story 3',
-  Year: 2010
-  })
+	Title: 'Toy Story 3',
+	Year: 2010
+})
  .toss()
 
 frisby.create('Post Movie - Error')
   .post('http://localhost:' + port + '/favourites?i=', { strictSSL: false })
     .expectStatus(500)
  .expectJSON({
-  message: "movie not found"
-  })
+	message: 'movie not found'
+})
  .toss()
- 
+
 
 frisby.create('Get Movie From omdb By ID')
   .get('http://localhost:' + port + '/movie?i=tt0114709', { strictSSL: false })
     .expectStatus(200)
  .expectJSON({
-   Title: 'Toy Story',
-   Year: '1995'
-  })
+	Title: 'Toy Story',
+	Year: '1995'
+})
  .toss()
 
 frisby.create('Get Movie From omdb By ID - Error')
   .get('http://localhost:' + port + '/movie?i=', { strictSSL: false })
     .expectStatus(200)
  .expectJSON({
-   Response: "False",
-   Error : "Something went wrong."
-  })
+	Response: 'False',
+	Error: 'Something went wrong.'
+})
  .toss()
 
 frisby.create('Search omdb')
   .get('http://localhost:' + port + '/search?s=Toy%20Story', { strictSSL: false })
     .expectStatus(200)
  .expectJSON('Search', [{
-  Title: 'Toy Story',
-  Year: '1995'
-  }])
+	Title: 'Toy Story',
+	Year: '1995'
+}])
  .toss()
 
 frisby.create('Search omdb Error')
   .get('http://localhost:' + port + '/search?s=', { strictSSL: false })
     .expectStatus(200)
  .expectJSON({
-  Response: "False",
-  Error : "Something went wrong."
-  })
+	Response: 'False',
+	Error: 'Something went wrong.'
+})
  .toss()
 
 
@@ -60,9 +60,9 @@ frisby.create('Retrieve Favourites List')
   .get('http://localhost:' + port + '/favourites', { strictSSL: false })
     .expectStatus(200)
  .expectJSON([{
-  Title: 'Toy Story 2',
-  Year: 1999
-  }])
+	Title: 'Toy Story 2',
+	Year: 1999
+}])
  .toss()
 
 /*frisby.create('Database Error')
@@ -79,52 +79,50 @@ frisby.create('Search By imdbId In Favourites')
   .get('http://localhost:8080/favourites/tt0435761', { strictSSL: false })
     .expectStatus(200)
  .expectJSON({
-  Title: 'Toy Story 3',
-  Year: 2010
-  })
+	Title: 'Toy Story 3',
+	Year: 2010
+})
  .toss()
 
 frisby.create('Search By imdbId In Favourites - Error')
   .get('http://localhost:8080/favourites/:id?id=', { strictSSL: false })
     .expectStatus(500)
  .expectJSON({
-  message: 'Id Not In Database'
-  })
+	message: 'Id Not In Database'
+})
  .toss()
 
 frisby.create('Update Rating For Movie')
  .put('http://localhost:8080/favourites/?id=tt0435761&rating=9.9', { strictSSL: false })
     .expectStatus(200)
  .expectJSON({
-  Title: 'Toy Story 3',
-  Year: 2010
-  })
+	Title: 'Toy Story 3',
+	Year: 2010
+})
  .toss()
 
 frisby.create('Update Rating For Movie - Error')
  .put('http://localhost:8080/favourites/?id=&rating=9.9', { strictSSL: false })
     .expectStatus(500)
  .expectJSON({
-  message: 'Not in Database'
-  })
+	message: 'Not in Database'
+})
  .toss()
 
 frisby.create('Delete Movie From Favourites - Error')
   .delete('http://localhost:8080/favourites/?id=', { strictSSL: false })
     .expectStatus(500)
   .expectJSON({
-   message: "Pass a valid id"
-  })
+	message: 'Pass a valid id'
+})
  .toss()
 
 frisby.create('Delete Movie From Favourites')
   .delete('http://localhost:8080/favourites/?id=tt0435761', { strictSSL: false })
     .expectStatus(200)
  .expectJSON({
-  ok: 1
-  })
+	ok: 1
+})
  .toss()
 
-
- 
 
--- modules/film.js
+++ modules/film.js
@@ -3,44 +3,45 @@ const request = require('request')
 const db = require('./persistence.js')
 
 exports.searchMovie = function(movieSearch, callback) {
-    if (movieSearch === undefined) {
-        callback(new Error('Pass In A Search'))
-    } else {
-	const search = {
-		method: 'GET'
+	if (movieSearch === undefined) {
+		callback(new Error('Pass In A Search'))
+	} else {
+		const search = {
+			method: 'GET'
         , url: 'http://www.omdbapi.com/'
         , qs: {
 	s: movieSearch
             , r: 'json'
 }
     , }
-	request(search, function(err, response, body) {
-		/* istanbul ignore next */	
-        if (err) {
-            callback(new Error(error))
-        } else {
-		return callback(null, JSON.parse(body))
-        }
-	})
-    }
-}   
+
+		request(search, function(err, response, body) {
+		/* istanbul ignore next */
+			if (err) {
+				callback(new Error(error))
+			} else {
+				return callback(null, JSON.parse(body))
+			}
+		})
+	}
+}
 
 exports.getMovie = function(input, type, year, callback) {
 	request(`http://www.omdbapi.com/?${type}=${input}&y=${year}`, function(err, response, body) {
-		/* istanbul ignore next */		
-        if (err) {
-            callback(new Error(error))
-        } else {
-		return callback(null, JSON.parse(body))
-        }
+		/* istanbul ignore next */
+		if (err) {
+			callback(new Error(error))
+		} else {
+			return callback(null, JSON.parse(body))
+		}
 	})
 }
 exports.addMovie = function(imdbID, callback) {
 	const url = `http://www.omdbapi.com/?i=${imdbID}`
 
 	request.get(url, (err, res, body) => {
-		/* istanbul ignore next */		
-        if (err) return callback(Error('could not complete request'))
+		/* istanbul ignore next */
+		if (err) return callback(Error('could not complete request'))
 		const json = JSON.parse(body)
 
 		if (json.Response === 'False'){
