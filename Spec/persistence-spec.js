06b32c1a18ef0b0982cce5716c3fc2fb6a9866fd
ramsayk3 <ramsayk3@uni.coventry.ac.uk>
2016-12-11 06:43:28 +0000

    Added New tests and tidied up original tests

--- Spec/persistence-spec.js
+++ Spec/persistence-spec.js
@@ -1,53 +1,65 @@
 'use strict'
 const persist = require('../modules/persistence')
-xdescribe('Persistence Module Testing', function () {
-    it('Show By ID', function (done) {
+
+describe('Persistence Module Testing', function () {
+    it('Should Return Movie By ID', function (done) {
         persist.showFavouritebyid('tt0120363', function (err, searchResult) {
             expect(err).toBe(null)
             expect(searchResult.Title).toBe('Toy Story 2')
             done()
         })
     })
-    it('Show By ID Error', function (done) {
-        persist.showFavouritebyid('tt0120', function (err, searchResult) {
+    it('Should Return Error As !DB', function (done) {
+        persist.showFavouritebyid('tt0120399', function (err, searchResult) {
+            expect(err.message).toBe("Id Not In Database")
+            expect(500)
+            done()
+        })
+    })
+    it('Should Return Error As Invalid ID', function (done) {
+        persist.showFavouritebyid('t', function (err, searchResult) {
             expect(err).toBe(err)
+            expect(500)
             done()
         })
     })
-    it('Remove Film', function (done) {
-        persist.remove('tt1233227', function (err, searchResult) {
-            expect(err).toBe(null)
-            done()
-        })
-    })
-    it('Remove Film Error', function (done) {
-        persist.remove('', function (err, searchResult) {
-            expect(err).toBe(err)
-            done()
-        })
-    })
-    it('Update Rating', function (done) {
-        persist.updaterating('tt0120363', 5, function (err, searchResult) {
+    it('Should Update Movie Rating', function (done) {
+        persist.updaterating('tt0120363', 8, function (err, searchResult) {
             expect(err).toBe(null)
             expect(searchResult.Title).toBe('Toy Story 2')
             done()
         })
     })
-    it('Update Rating Error', function (done) {
+    it('Should Fail To Update As !DB', function (done) {
+        persist.updaterating('tt0120399', 5, function (err, searchResult) {
+            expect(err.message).toBe("Not in Database")
+            expect(500)
+            done()
+        })
+    })
+    it('Should Fail To Update As Invalid ID', function (done) {
         persist.updaterating('', 5, function (err, searchResult) {
-            expect(err).toBe(err)
+            expect(err.message).toBe('Pass a valid id')
+            expect(500)
             done()
         })
     })
-    xit('Should return favourites', function (done) {
-        persist.showFavourites('', function (err, searchResult) {
+    it('Should Return Movies In Favourites', function (done) {
+        persist.showFavourites(function (err, searchResult) {
+            expect(err).toBe(null)
+            expect(searchResult[0].Title).toBe('Toy Story 2')
+            done()
+        })
+    })
+    it('Should Remove Film From DB', function (done) {
+        persist.remove('tt0385705', function (err, searchResult) {
             expect(err).toBe(null)
             done()
         })
     })
-    xit('Should return favourites', function (done) {
-        persist.showFavourites(123, function (err, searchResult) {
-            expect(err).toBe(null)
+    it('Should Fail To Remove Film As Invalid ID', function (done) {
+        persist.remove('', function (err, searchResult) {
+            expect(err.message).toBe('Pass a valid id')
             done()
         })
     })
